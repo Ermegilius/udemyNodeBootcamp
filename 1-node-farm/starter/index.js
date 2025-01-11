45f2f655d86e4585928a2f42e13b2d63e7f34900
Vladimir Beliakov <ermegilius@gmail.com>
2025-01-11 10:29:02 +0200

    finish lesson 10

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,32 @@
+"use-strict";
+
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+//Blocking, syncronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+//Non-blocking asyncronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    if (err) return console.log("Error! 💥");
+
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        console.log(data2);
+        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile(
+                "./txt/final.txt",
+                `${data2}\n${data3}`,
+                "utf-8",
+                (err) => {
+                    console.log("Your file has been written! 👌");
+                }
+            );
+        });
+    });
+});
+console.log("Will read the file! 📖");
