4d0f5e51fd5cf3812fcdc044350f3cadff3dc44b
Vladimir Beliakov <ermegilius@gmail.com>
2025-01-11 10:43:23 +0200

    finish lesson 11

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,10 @@
 "use-strict";
 
 const fs = require("fs");
+const http = require("http");
+
+/////////////////////////////////////////
+//FILES
 
 //Blocking, syncronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -10,23 +14,34 @@ const fs = require("fs");
 // console.log("File written!");
 
 //Non-blocking asyncronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-    if (err) return console.log("Error! 💥");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//     if (err) return console.log("Error! 💥");
 
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-        console.log(data2);
-        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-            console.log(data3);
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//         console.log(data2);
+//         fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile(
-                "./txt/final.txt",
-                `${data2}\n${data3}`,
-                "utf-8",
-                (err) => {
-                    console.log("Your file has been written! 👌");
-                }
-            );
-        });
-    });
+//             fs.writeFile(
+//                 "./txt/final.txt",
+//                 `${data2}\n${data3}`,
+//                 "utf-8",
+//                 (err) => {
+//                     console.log("Your file has been written! 👌");
+//                 }
+//             );
+//         });
+//     });
+// });
+// console.log("Will read the file! 📖");
+
+/////////////////////////////////////////
+//SERVER
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end("Hello from the server!");
 });
-console.log("Will read the file! 📖");
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Server is listening on port 8000");
+});
